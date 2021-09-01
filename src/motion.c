98c3261659ddfbc19f8e1d8fcadae90b2007f0b3
John Selmys <selmys@lotuspond.ca>
2021-09-01 11:37:13 -0400

    added headers and globals

--- src/motion.c
+++ src/motion.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include "pins.h"
 
-// Motion Sensor defines
-#define MOTION_LEFT 10
-#define MOTION_RIGHT 9
+unsigned long last_time_bumper_hit = 0;
+unsigned long last_time_motion_sensed = 0;
+int ml=0,mr=0;
 
 void MotionLeft(void) {
     unsigned long interrupt_time = millis();
     if (interrupt_time - last_time_motion_sensed > 200) {
         printf("Motion Left Detected!\n");
-        SaySomething("Danger! Attack left!");
     }
     last_time_motion_sensed = interrupt_time;
     ml=1;
@@ -17,7 +19,6 @@ void MotionRight(void) {
     unsigned long interrupt_time = millis();
     if (interrupt_time - last_time_motion_sensed > 200) {
         printf("Motion Right Detected!\n");
-        SaySomething("Danger! Attack right!");
     }
     last_time_motion_sensed = interrupt_time;
     mr=1;
